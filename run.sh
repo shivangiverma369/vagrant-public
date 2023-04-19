81f0e3a507c0f11c983ab1fc60a4840e09669c47
shivangi <vshivangi51@gmail.com>
2023-04-19 11:06:21 +0530

    first commit

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 set -x
 set -e
 
+ubuntu="/home/main/Desktop/vagrant/ubuntu"
+redhat="/home/main/Desktop/vagrant/redhat"
+
 repoRoot=$(realpath "$(dirname "$0")")
 
 cd "$repoRoot"
@@ -17,69 +20,34 @@ fi
 
 case "$COMPONENT" in
 ubuntu)
-    mkdir -p "/home/main/Desktop/vagrant/ubuntu"
-    cd "/home/main/Desktop/vagrant/ubuntu"
-    curl -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant/main/ubuntu/Vagrantfile
-    # https://raw.githubusercontent.com/ankursharma2725/jenkins-vagrant/main/Vagrantfile
-#     curl -LJO https://github.com/ankursharma2725/jenkins-vagrant/main/Vagrantfile
-#     sudo -u main vagrant up
-#     chmod 777 Vagrantfile
+    mkdir -p $ubuntu
+    cd $ubuntu
+    curl -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant-public/main/ubuntu/Vagrantfile
     vagrant up
     ;;
 ubuntu_clean-up)
-    cd "/home/main/Desktop/vagrant"
+    cd $ubuntu
     vagrant halt && vagrant destroy -f
     ;;
 redhat)
-    mkdir -p "/home/main/Desktop/vagrant/redhat"
-    cd "/home/main/Desktop/vagrant/redhat"
-    curl -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant/main/redhat/Vagrantfile
+    mkdir -p $redhat
+    cd $redhat
+    curl -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant-public/main/redhat/Vagrantfile
     vagrant up
     ;;
 redhat_clean-up)
-    cd "/home/main/Desktop/vagrant/redhat"
+    cd $redhat
     vagrant halt && vagrant destroy -f
     ;;
-reset-vm)
-    mkdir "home/main/Desktop/vagrant/vagrant-reset"
-    cd "/home/main/Desktop/vagrant/vagrant-reset"
-    curl -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant/main/redhat/Vagrantfile
-    vagrant up --vagrantfile vagrant-reset
-
-
-    ;;
-kibana)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-kibana.yml --connection=local
-    ;;
-configurator)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-configurator.yml --connection=local 
-    ;;
-heartbeat)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-heartbeat.yml --connection=local 
-    ;;
-metricbeat)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-metricbeat.yml --connection=local 
-    ;;
-filebeat)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-filebeat.yml --connection=local 
-    ;;
-auditbeat)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-auditbeat.yml --connection=local 
-    ;;                         
-kafka)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-kafka.yml --connection=local 
-    ;;
-kafka-ui)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-kafka-ui.yml --connection=local 
-    ;;    
-promethues)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-promethues.yml --connection=local 
-    ;;
-grafana)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-grafana.yml --connection=local 
-    ;;
-jenkins)
-    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-jenkins.yml --connection=local 
-    ;;             
+reset-ubuntu)
+    cd $ubuntu
+    vagrant halt && vagrant destroy -f 
+    vagrant up
+   ;;
+reset-redhat)
+    cd $redhat
+    vagrant halt && vagrant destroy -f 
+    vagrant up
+   ;;         
 *) ;;
 esac
